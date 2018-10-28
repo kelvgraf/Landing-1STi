4b48b486d6605c0636aa1c91e30e1caac89a0798
Kelvin <kelvin.f.almeida@gmail.com>
2018-10-28 19:43:48 -0300

    fix adjust desktop and developing responsive

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -7,6 +7,8 @@
   document.getElementById("menu-item3").addEventListener("click", myFunction2);
   document.getElementById("menu-item4").addEventListener("click", myFunction2);
   document.getElementById("menu-item5").addEventListener("click", myFunction2);
+
+  const home = document.getElementsByClassName("home-content")
  
   function myFunction() {
     menuList.classList.toggle('--drop-menu')
@@ -18,3 +20,78 @@
   function myFunction2() {
     menuList.classList.remove('--drop-menu')
   };
+
+console.log('home', home)
+
+function isShow() {
+
+}
+
+function animateTitles() {
+    let counter = 0;
+    const specialChars = ['$','%','#','@','&','(',')','=','*','/'];
+    const totalSpecialChars = specialChars.length;
+    const getRandom = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+    const work = document.querySelector('.container-title-work');
+    const approach = document.querySelector('.container-title-approach');
+    const cases = document.querySelector('.container-title-cases');
+    const time = document.querySelector('.container-title-time');
+    const conatct = document.querySelector('.container-title-contact');
+
+    generateLetters(work)
+    generateLetters(approach)
+    generateLetters(cases)
+    generateLetters(time)
+    generateLetters(conatct)
+
+
+
+    const workLetters = document.querySelectorAll('.container-title-work .title__letter');
+    const approachLetters = document.querySelectorAll('.container-title-approach .title__letter');
+    const casesLetters = document.querySelectorAll('.container-title-cases .title__letter');
+    const timeLetters = document.querySelectorAll('.container-title-time .title__letter');
+    const contactLetters = document.querySelectorAll('.container-title-contact .title__letter');
+    
+    function generateLetters (element) {
+      return element.innerHTML = element.innerHTML.split('').map(string => {
+        return `<span class="title__letter" data-letter="${string}">${string}</span>`
+      }).join('')
+    }
+
+    function writeLetters(letters) {
+      letters.forEach((letter, index) => {
+        let timeout = setTimeout(() => {
+          if (letter.innerHTML === ' ') {
+            letter.innerHTML = ' '
+          } else {
+            letter.innerHTML = specialChars[getRandom(0, totalSpecialChars - 1)]
+          }
+          
+          setTimeout(() => {
+            letter.innerHTML = letter.getAttribute('data-letter');
+          }, 100)
+        }, index * 200)
+      });
+    }
+
+    function loopLetters(letters, ms) {
+      setInterval(() => {
+        writeLetters(letters)
+      }, ms)
+    }
+
+    writeLetters(workLetters)
+    writeLetters(approachLetters)
+    writeLetters(casesLetters)
+    writeLetters(timeLetters)
+    writeLetters(contactLetters)
+
+    loopLetters(workLetters, 5000)
+    loopLetters(approachLetters, 5000)
+    loopLetters(casesLetters, 4000)
+    loopLetters(timeLetters, 4000)
+    loopLetters(contactLetters, 4000)
+  }
+
+  animateTitles();
