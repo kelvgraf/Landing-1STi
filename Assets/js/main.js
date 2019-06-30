bf27d630d8db287644ef0930f679f4236f14ad44
Kelvin Almeida <kelvin.f.almeida@gmail.com>
2019-06-30 19:59:24 -0300

    Adjustments and hotfix in  text hello, change in cases structure and ecosystem

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -137,22 +137,23 @@
       }
   };
 
-  var slideIndex = 1;
+
+  // var slideIndex = 1;
 		
-  showDivs(slideIndex);
-  function plusDivs(n) {
-    showDivs(slideIndex += n);
-  }
-  function showDivs(n) {
-    var i;
-    var x = document.getElementsByClassName("acao-slide");
-    if (n > x.length) {slideIndex = 1}    
-    if (n < 1) {slideIndex = x.length}
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";  
-    }
-    x[slideIndex-1].style.display = "block";  
-  }
+  // showDivs(slideIndex);
+  // function plusDivs(n) {
+  //   showDivs(slideIndex += n);
+  // }
+  // function showDivs(n) {
+  //   var i;
+  //   var x = document.getElementsByClassName("acao-slide");
+  //   if (n > x.length) {slideIndex = 1}    
+  //   if (n < 1) {slideIndex = x.length}
+  //   for (i = 0; i < x.length; i++) {
+  //       x[i].style.display = "none";  
+  //   }
+  //   x[slideIndex-1].style.display = "block";  
+  // }
 
   
 function animeScroll() {
